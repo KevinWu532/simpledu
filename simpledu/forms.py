3d273bfbce6e79600461a8aac07379052052c00e
KevinWu532 <wujing532@outlook.com>
2019-02-12 06:36:08 +0800

    更新

--- simpledu/forms.py
+++ simpledu/forms.py
@@ -8,7 +8,7 @@ from wtforms.validators import Length, Email, EqualTo, DataRequired
 
 class RegisterForm(FlaskForm):
     username = StringField('用户名', validators=[DataRequired(), Length(3, 24)])
-    email = StringField('邮箱', validators=[DataRequired(), Email()])
+    email = StringField('邮箱', validators=[DataRequired(), Email(message='请输入正确的邮箱地址')])
     password = PasswordField('密码', validators=[DataRequired(), Length(6, 24, message='密码长度要在6-24个字符之间')])
     repeat_password = PasswordField('重复密码', validators=[DataRequired(), EqualTo('password', message='密码不一致')])
     submit = SubmitField('提交')
@@ -39,8 +39,6 @@ class LoginForm(FlaskForm):
     password = PasswordField('密码', validators=[DataRequired(), Length(6, 24, message='')])
     remember_me = BooleanField('记住我')
     submit = SubmitField('提交')
-
-
 
     def validate_username(self, field):
         temp = len(re.findall('[a-zA-Z_0-9]', field.data))
